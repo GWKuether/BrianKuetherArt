1e0dced4463abd7962ff8c800a897a0b0d7e8832
George Kuether <gwkuether@gmail.com>
2023-03-14 16:08:31 -0500

    Added Formatting and Styling to Originals Page

--- frontend/src/Pages/OriginalsPage/OriginalsPage.js
+++ frontend/src/Pages/OriginalsPage/OriginalsPage.js
@@ -1,51 +1,56 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-
+import "./OriginalsPage.css"
 
 const OriginalsPage = (props) => {
+  const [artWork, setArtWork] = useState([]);
 
-    const [artWork, setArtWork] = useState([])
+  useEffect(() => {
+    fetchArtwork();
+  }, []);
 
-    useEffect(() => {
-        fetchArtwork();
-      }, []);
+  const fetchArtwork = async () => {
+    let response = await axios.get("http://127.0.0.1:8000/api/art");
+    setArtWork(response.data);
+  };
 
-    const fetchArtwork = async () => {
-        let response = await axios.get("http://127.0.0.1:8000/api/art")
-        setArtWork(response.data)
-    }
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  function handleImageClick(image) {
+    let imageID = {
+      imageID: image.id,
+      imageTitle: image.title,
+      imageLocation: image.image,
+      imagePrice: image.price,
+      imageSold: image.sold,
+      imageSize: image.size,
+    };
+    navigate("/piece", { state: imageID });
+  }
 
-    function handleImageClick(image){
-        let imageID = {
-            imageID : image.id,
-            imageTitle: image.title,
-            imageLocation: image.image,
-            imagePrice: image.price,
-            imageSold: image.sold,
-            imageSize: image.size
-          };
-        navigate("/piece", { state: imageID });
-    }
+  function displayArt() {
+    let results = artWork.map((image) => {
+      return (
+          <div className="box">
+            <h4 style={{display: "flex", justifyContent: "center"}}>{image.title}</h4>
+            <img
+              style={{ height: "15em", width: "15em", margin: "1em" }}
+              onClick={() => handleImageClick(image)}
+              src={image.image}
+              alt="art"
+            ></img>
+          </div>
+      );
+    });
+    return results;
+  }
 
+  return (
+    <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-evenly", margin: "1em"}}>
+      {displayArt()}
+    </div>
+  );
+};
 
-    function displayArt(){
-        let results = artWork.map((image) =>{
-            return <img style={{height: "10em", width:"10em"}} onClick={() => handleImageClick(image)} src={image.image} alt="art" ></img>
-        })
-        return results
-    }
-
-
-
-    return ( 
-        <div>
-        <p>This is the originals page</p>
-        {displayArt()}
-        </div>
-     );
-}
- 
 export default OriginalsPage;
